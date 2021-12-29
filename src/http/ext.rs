fbb3970530bd8d9439e801f3ccb3fdda18375442
Ibraheem Ahmed <ibrah1440@gmail.com>
2021-12-28 22:36:36 -0500

    add arbitrary value caching

--- src/http/ext.rs
+++ src/http/ext.rs
@@ -1,3 +1,8 @@
+use std::any::{Any, TypeId};
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+use once_cell::sync::OnceCell;
+
 use super::{RequestBuilder, ResponseBuilder};
 use crate::{state, Request, Response, State};
 
@@ -5,6 +10,18 @@ use crate::{state, Request, Response, State};
 pub trait RequestExt {
     /// Returns the route parameter with the given name.
     fn param(&self, name: impl AsRef<str>) -> Option<&str>;
+
+    /// Caches the provided value and returns a reference to it.
+    ///
+    /// Cached values can be later retrieved with [`RequestExt::cached`].
+    fn cache<T>(&self, value: T) -> &T
+    where
+        T: State;
+
+    /// Retrieves a value of type `T` if one has been previously [cached](RequestExt::cache).
+    fn cached<T>(&self) -> Option<&T>
+    where
+        T: State;
 
     /// Rerieves application state from the request.
     ///
@@ -19,6 +36,20 @@ impl RequestExt for Request {
         self.extensions()
             .get::<Params>()
             .and_then(|params| params.get(name))
+    }
+
+    fn cache<T>(&self, value: T) -> &T
+    where
+        T: State,
+    {
+        self.extensions().get::<Cache>().unwrap().insert(value)
+    }
+
+    fn cached<T>(&self) -> Option<&T>
+    where
+        T: State,
+    {
+        self.extensions().get::<Cache>().unwrap().get::<T>()
     }
 
     fn state<T>(&self) -> Option<&T>
@@ -91,5 +122,62 @@ impl FromIterator<(String, String)> for Params {
         T: IntoIterator<Item = (String, String)>,
     {
         Params(iter.into_iter().collect())
+    }
+}
+
+const ITEMS: usize = 8;
+
+#[derive(Default)]
+pub(crate) struct Cache {
+    items: [OnceCell<(TypeId, Box<dyn Any + Send + Sync>)>; ITEMS],
+    len: AtomicUsize,
+}
+
+impl Cache {
+    fn get<T>(&self) -> Option<&T>
+    where
+        T: State,
+    {
+        let id = TypeId::of::<T>();
+
+        self.items
+            .iter()
+            .take(self.len.load(Ordering::Acquire))
+            // SAFETY: `self.len` are initialized
+            .map(|cell| unsafe { cell.get_unchecked() })
+            .find(|(i, _)| *i == id)
+            .map(|(_, value)| value.downcast_ref().unwrap())
+    }
+
+    fn insert<T>(&self, value: T) -> &T
+    where
+        T: State,
+    {
+        let i = self.len.fetch_add(1, Ordering::AcqRel);
+        self.items[i]
+            .set((TypeId::of::<T>(), Box::new(value)))
+            .unwrap();
+
+        // SAFETY: we just set the value
+        let (_, value) = unsafe { self.items[i].get_unchecked() };
+        value.downcast_ref().unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_cache() {
+        let cache = Cache::default();
+        assert_eq!(cache.get::<()>(), None);
+        cache.insert(42_usize);
+        cache.insert("42");
+        cache.insert("42".to_string());
+        assert_eq!(cache.get::<usize>(), Some(&42));
+        assert_eq!(cache.get::<&str>(), Some(&"42"));
+        assert_eq!(cache.get::<String>().map(String::as_str), Some("42"));
+        assert_eq!(cache.get::<()>(), None);
     }
 }
